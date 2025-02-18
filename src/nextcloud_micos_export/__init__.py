df3a732a86352a8cc34e0e7c352e386de03fb5c7
Julius Koenig <julius@koenig-site.de>
2025-02-18 17:21:22 +0100

    Update project metadata and standardize naming conventions
    Updated author information to use the company name and general email. Standardized package naming by replacing underscores with hyphens. Added a short project description to the README for improved clarity.

--- src/nextcloud_micos_export/__init__.py
+++ src/nextcloud_micos_export/__init__.py
@@ -1,9 +1,9 @@
 __title__ = "Nextcloud Micos Export"
-__name__ = "nextcloud_micos_export"
+__name__ = "nextcloud-micos-export"
 __description__ = "Move files from input to output folder and delete old files from output folder."
 __license__ = "GPL-3.0"
-__author__ = "Julius Koenig"
-__author_email__ = "julius.koenig@kds-kg.de"
+__author__ = "Kirchhoff Datensysteme Services GmbH & Co. KG"
+__author_email__ = "service@kds-kg.de"
 __version__ = "0.5.2"
 
 from nextcloud_micos_export.Settings import Settings
